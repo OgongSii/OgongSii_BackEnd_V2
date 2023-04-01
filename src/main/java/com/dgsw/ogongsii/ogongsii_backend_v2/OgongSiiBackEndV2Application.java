657a3707ceafa23954be1d09a3daf9f9837df09c
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-01 18:29:58 +0900

    :tada: :: v2 start!!

---
+++ src/main/java/com/dgsw/ogongsii/ogongsii_backend_v2/OgongSiiBackEndV2Application.java
@@ -0,0 +1,13 @@
+package com.dgsw.ogongsii.ogongsii_backend_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgongSiiBackEndV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgongSiiBackEndV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/ogongsii/ogongsii_backend_v2/OgongSiiBackEndV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.ogongsii.ogongsii_backend_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgongSiiBackEndV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
