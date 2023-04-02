e0f41f189fb7bde04ed9235b98e9178753a5333a
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-02 20:15:14 +0900

    :art: :: Impl 구조 삭제

--- src/main/java/com/dgsw/ogongsii/ogongsii_backend_v2/domain/record/service/RecordService.java
+++ src/main/java/com/dgsw/ogongsii/ogongsii_backend_v2/domain/record/service/RecordService.java
@@ -1,12 +1,41 @@
 package com.dgsw.ogongsii.ogongsii_backend_v2.domain.record.service;
 
 import com.dgsw.ogongsii.ogongsii_backend_v2.domain.record.domain.Record;
+import com.dgsw.ogongsii.ogongsii_backend_v2.domain.record.domain.repository.RecordRepository;
 import com.dgsw.ogongsii.ogongsii_backend_v2.domain.record.presentation.dto.request.RecordRequest;
+import com.dgsw.ogongsii.ogongsii_backend_v2.domain.user.domain.Member;
+import com.dgsw.ogongsii.ogongsii_backend_v2.domain.user.domain.repository.MemberRepository;
+import com.dgsw.ogongsii.ogongsii_backend_v2.domain.user.exception.MemberNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface RecordService {
+@Service
+@RequiredArgsConstructor
+public class RecordService {
 
-    public void record(Record record);
+    private final RecordRepository recordRepository;
 
-    public void saveRecord(String name, RecordRequest request) throws Exception;
+    private final MemberRepository memberRepository;
 
+
+    public void record(Record record) {
+        recordRepository.save(record);
+    }
+
+    @Transactional
+    public void saveRecord(String name, RecordRequest request) throws Exception {
+
+        Member member = memberRepository.findByName(name)
+                .orElseThrow(MemberNotFoundException::new);
+
+        Record record = Record.builder()
+                .h(request.getH())
+                .m(request.getM())
+                .member(member)
+                .build();
+
+        member.addRecord(record);
+        memberRepository.save(member);
+    }
 }
